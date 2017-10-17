f2017d049ff20a0c5f0dfa77c3a2786a5310c2bb
mattia-lecci <mattia.lecci@gmail.com>
2017-10-17 18:52:22 +0200

    Started extractBatchFeatures

--- Code/main.m
+++ Code/main.m
@@ -6,15 +6,31 @@ clc
 % paths
 addpath('init')
 
+% parameters
+trainPerc = .7;
+
+% flags
 performFeatureExtraction = true;
 
+% feature extraction
 if performFeatureExtraction
     % extract names and chords labels
     chordTable = getNameChordTable('wavs');
     batchVec12 = extractBatchFeatures(names,featureType)
     % should we save them? one per each feature type. computed only once and
     % for all. if so, wrap in if
+    save('Save/init',{'chordTable','batchVec12'})
+else
+    load 'Save/init';
 end
+
+% train/test separation
+Ntot = size(chordTable,1);
+
+[trainIdx,~,testIdx] = dividerand(Ntot,trainPerc,0,1-trainPerc);
+trainIdx = trainIdx(randperm(length(trainIdx)));
+testIdx = testIdx(randperm(length(testIdx)));
+
 %% training
 % train GMM and SVM once and save their objects containing settings
 trainedObj = trainModel(modelType,trainChords,trainVec12,name-value_pairs)
