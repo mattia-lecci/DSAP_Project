05585f06bc2564a401f861d832872419ee0156db
FedMason <federicomason94@hotmail.it>
2017-12-13 12:03:29 +0100

    Other plotted results

--- Code/error_comparison_hmm.m
+++ Code/error_comparison_hmm.m
@@ -1,23 +1,23 @@
 clear all
 close all
 
-load 'Result/CRP/Test03Train07#1'
+load 'Result/CENS/Test03Train07#1'
 
-plot(errors(2,:));
+for i=1:size(errors,2)
+    err(i,1) = errors(2,i);
+    err(i,2) = errors(1,i);
+end
+
+bar(err);
 
 grid on;
 
-ylim([0 1.2]);
-xlim([1 33]);
+ylim([0 1.2]); xlim([0 34]);
 
 ylabel('Error');
 xlabel('Song');
 
 title('Error comparison');
-
-hold on;
-
-plot(errors(1,:));
 
 legend('Before processing HMM', 'After processing HMM');
 
